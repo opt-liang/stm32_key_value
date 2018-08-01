fb299c23d360ceb2ccf5070e7dec97975043a4db
yanzi418 <yanzi418@users.noreply.github.com>
2018-08-01 21:08:50 +0800

    Update key_value.h

--- key_value.h
+++ key_value.h
@@ -1,10 +1,14 @@
 #ifndef __KEY_VALUE__
 #define __KEY_VALUE__
+#include <stdint.h>
+#include <stdbool.h>
+#include "insideflash.h"
 enum TYPE{
     UINT32   ,
     STRINGS  ,
 };
-
+void key_value_test( void );
+void check_hash_conflict( int count,...);
 bool get_key_value( char *key, enum TYPE type , uint8_t *value );
 bool set_key_value( char *key, enum TYPE type, uint8_t *value );
 void init_key_value( uint32_t key_value_int32, uint32_t key_value_string, uint32_t key_value_backup );
