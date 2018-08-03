4bdf2ce27c27d793b32c3076b6ee528de0430188
yanzi418 <yanzi418@users.noreply.github.com>
2018-08-03 10:33:43 +0800

    Update key_value.h

--- key_value.h
+++ key_value.h
@@ -13,4 +13,3 @@ bool get_key_value( char *key, enum TYPE type , uint8_t *value );
 bool set_key_value( char *key, enum TYPE type, uint8_t *value );
 void init_key_value( uint32_t key_value_int32, uint32_t key_value_string, uint32_t key_value_backup );
 #endif
-
