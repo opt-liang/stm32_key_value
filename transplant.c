28032ac94fdb62f61d7a3669394bc2e22c61132e
yanzi418 <yanzi418@users.noreply.github.com>
2018-08-06 09:38:38 +0800

    Create transplant.c

---
+++ transplant.c
@@ -0,0 +1,52 @@
+#include "transplant.h"
+
+
+uint32_t flash_sector_address( int16_t index ){
+    
+    if( index > SECTOR_NUM ){
+        printf( "Fan area index error\r\n" );
+        while( true );
+    }
+    
+    uint32_t realaddr = FLASH_BASE;
+    
+    #if defined M3
+        realaddr += ( index ) * KEY_VALUE_SIZE;
+    #else
+        //stm32f407ve       16 16 16 16 64 128 128 128
+        if( index > 0 ){
+            realaddr += ( index > 3 ? 4 : index ) * 16 * 1024;
+        }
+        if( index > 4 ){
+            realaddr += 1 * 64 * 1024;
+        }
+        if( index > 5 ){
+            realaddr += ( index - 5 ) * 128 * 1024;
+        }
+    #endif
+    
+    return realaddr;
+}
+
+bool flash_legal_sector_address( int32_t flashaddr ){
+    for( uint16_t i = 0; i < SECTOR_NUM; i++ ){
+        if( flashaddr == flash_sector_address( i ) ){
+            return true;
+        }
+    }
+    return false;
+}
+
+int16_t flash_sector_index( uint32_t flashaddr ){
+    for( int16_t i = 0; i < SECTOR_NUM; i++ ){
+        if( flashaddr == flash_sector_address( i ) ){
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+
+
+
