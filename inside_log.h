bb832eebedeedfd84998cd76a74b0f4ae4a3a575
yanzi418 <yanzi418@users.noreply.github.com>
2018-08-06 09:35:52 +0800

    Delete inside_log.h

--- inside_log.h
+++
@@ -1,9 +0,0 @@
-#ifndef __INSIDE_LOG__
-#define __INSIDE_LOG__
-
-bool UpdateCurrentLogValue( uint32_t flash_addr, int int_variable );
-bool GetCurrentLogValue( uint32_t flash_addr, uint32_t* int_variable );
-void update_stack_value( void );
-void stack_history_use_maximum( void );
-void reboot_times_history_info( void );
-#endif
