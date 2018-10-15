863666acb4e00cc2440e384330c4f6008192b52b
yanzi418 <yanzi418@users.noreply.github.com>
2018-10-15 17:59:52 +0800

    Update key_value.h

--- key_value.h
+++ key_value.h
@@ -11,5 +11,5 @@ void key_value_test( void );
 void check_hash_conflict( int count,...);
 bool get_key_value( char *key, enum TYPE type , uint8_t *value );
 bool set_key_value( char *key, enum TYPE type, uint8_t *value );
-void init_key_value( uint32_t key_value_int32, uint32_t key_value_string, uint32_t key_value_backup );
+void init_key_value( uint8_t key_value_int32_flash_index, uint8_t key_value_string_flash_index, uint8_t key_value_backup_flash_index );
 #endif
