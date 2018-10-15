fc4c51b03a5cf2858d5ae293e9e1bf3695c2f706
yanzi418 <yanzi418@users.noreply.github.com>
2018-10-15 17:53:50 +0800

    Update transplant.c

--- transplant.c
+++ transplant.c
@@ -1,5 +1,23 @@
 #include "transplant.h"
 
+#define ITM_Port8(n)    (*((volatile unsigned char *)(0xE0000000+4*n)))
+#define ITM_Port16(n)   (*((volatile unsigned short*)(0xE0000000+4*n)))
+#define ITM_Port32(n)   (*((volatile unsigned long *)(0xE0000000+4*n)))
+
+#define DEMCR           (*((volatile unsigned long *)(0xE000EDFC)))
+#define TRCENA          0x01000000
+
+struct __FILE { int handle; /* Add whatever you need here */ };
+FILE __stdout;
+FILE __stdin;
+
+int fputc(int ch, FILE *f) {
+  if (DEMCR & TRCENA) {
+    while (ITM_Port32(0) == 0);
+    ITM_Port8(0) = ch;
+  }
+  return(ch);
+}
 
 uint32_t flash_sector_address( int16_t index ){
     
